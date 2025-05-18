84ac1a17812f56a5760c9b433a4e7d8886bef47e
Yi Ding <yi.s.ding@gmail.com>
2025-05-18 15:51:06 -0700

    remove TypeScript to fix build

--- pages/api/tag.js
+++ pages/api/tag.js
@@ -1,9 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).end();
   }
